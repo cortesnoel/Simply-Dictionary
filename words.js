0405976cc02ae5baff49c5f7d7e8c74fe337a2d9
Noel Cortes <noelcortes472640@gmail.com>
2019-01-11 22:59:00 -0500

    Fixing end-user bugs and improving code abstraction.

--- words.js
+++ words.js
@@ -12,27 +12,77 @@ $.ajax({
   dataType: "json",
   success: (data)=>{
     console.log(data);
-    $("li").remove();
-    $("#image").removeAttr("src");
+
+    if(isDataEmpty(data)) {
+      return;
+    }
+
+    clearDefinitions();
+
     for(i=(data.length-1);i>-1;i--) {
-      for(x=(data[i].shortdef.length-1);x>-1;x--) {
-        console.log(data[i].shortdef[x]);
-        $("#definitionList").after("<br><li><b>"+data[i].fl+":</b> "+data[i].shortdef[x]+"</li>");
-        $("li").hide();
+      if((data[i].shortdef) === undefined) {
+        displaySuggestions(data[i]);
+      }else{
+        displayDefinitions(data[i]);
       }
     }
-    $("li").show(1000);
 
-    if(data[0].art) {
-      $(".picture").removeAttr("hidden");
-      $("#image").attr("src", "https://www.merriam-webster.com/assets/mw/static/art/dict/"+data[0].art.artid+".gif").hide().delay(100).show(1200);
-      $("#image").attr("title", data[0].hwi.hw);
-    }else{
-      $(".picture").attr("hidden","hidden");
-    }
+    $("li").show(1000);
+    displayPicture(data);
   },
   error: (xhr,status,error)=>{
     alert("Error: " + errorMessage);
   }
 });
 };
+
+function isDataEmpty (data) {
+  if ($.isEmptyObject(data)) {
+    clearDefinitions();
+    $("#definitionList").after("<h1 id='noSuggestions'>Sorry, we couldn't find your definition(s).<br>Please search again.</h1>");
+    console.log("Arrived on --> isDataEmpty() = true");
+
+    return true;
+  }
+  
+  console.log("Arrived on --> isDataEmpty() = false");
+};
+
+function clearDefinitions() {
+  console.log("Arrived on --> clearDefinitions()");
+
+  $("li").remove();
+  $("br").remove();
+  $("#noSuggestions").remove();
+  $(".picture").attr("hidden","hidden");
+  $("#image").removeAttr("src");
+}
+
+function displaySuggestions(data) {
+  console.log("Arrived on --> displaySuggestions()");
+
+  $("#definitionList").after("<br><li><b>Suggestion:</b> "+data+"</li>");
+  $("li").hide();
+}
+
+function displayDefinitions(data) {
+  console.log("Arrived on --> displayDefinitions()");
+
+  for(x=(data.shortdef.length-1);x>-1;x--) {
+    console.log(data.shortdef[x]);
+    $("#definitionList").after("<br><li><b>"+data.fl+":</b> "+data.shortdef[x]+"</li>");
+    $("li").hide();
+  }
+}
+
+function displayPicture(data) {
+  console.log("Arrived on --> displayPicture()");
+
+  if(data[0].art) {
+      $(".picture").removeAttr("hidden");
+      $("#image").attr("src", "https://www.merriam-webster.com/assets/mw/static/art/dict/"+data[0].art.artid+".gif").hide().delay(100).show(1200);
+      $("#image").attr("title", data[0].hwi.hw);
+  }else{
+    $(".picture").attr("hidden","hidden");
+  }
+}
